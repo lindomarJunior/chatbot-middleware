2a01869e757af924b33e4d3f4a6c3391ca7197f1
Lindomar Junior <lindomar.junior.6@gmail.com>
2022-01-24 08:47:05 -0300

    teste

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const venom = require('venom-bot');
+
+venom
+  .create({
+    session: 'session-name', //name of session
+    multidevice: false // for version not multidevice use false.(default: true)
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onAnyMessage((message) => {
+    console.log("teste2");
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome Venom 🕷')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
+
+console.log("teste1");
+
