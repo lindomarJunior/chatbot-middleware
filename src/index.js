86410aae60998701e96088a8dd8054e45bdd89ce
Lindomar Junior <lindomar.junior.6@gmail.com>
2022-01-24 12:16:09 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ function start(client) {
   });
 }
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   onsole.log(`Our app is running on port ${ PORT }`);
